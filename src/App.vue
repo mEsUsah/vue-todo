bddb644de24d818e5f3d6af8b1726a0ddeecb634
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-06-30 21:38:22 +0200

    add Header

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <the-header title="Todo's"></the-header>
+</template>
+
+<script>
+
+import TheHeader from './components/TheHeader.vue'
+export default {
+  components: { TheHeader },
+
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;700&display=swap');
+body{
+    margin: 0;
+    font-family: 'Lato', sans-serif;
+    background-color: rgb(182, 182, 182);
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    props: ['title']
+}
+</script>
+
+<style scoped>
+header{
+    background-color: rgb(0, 194, 0);
+}
+h1{
+    margin: 0;
+    padding: 20px;
+    color: black;
+}
+</style>
