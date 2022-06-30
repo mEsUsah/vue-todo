f9c73099214455b6aab1ee96104abeffb21cff79
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-06-30 21:47:39 +0200

    add BaseCard

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="base-card">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default{
+}
+</script>
+
+<style scoped>
+    div{
+        background-color: rgb(0, 194, 0);
+        padding: 20px;
+        margin: 0 auto;
+        max-width: 800px;
+        widows: 100%;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import BaseCard from './components/BaseCard.vue';
+
 const app = createApp(App);
 app.component(App);
+app.component(BaseCard);
 app.mount('#app');
