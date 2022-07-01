0e49a17bd89dec21caf03f61cac34fb9e90fcce7
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-07-02 00:10:52 +0200

    BaseCard: add modifier and some styling

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,20 +1,26 @@
 <template>
-    <div class="base-card">
+    <div :class="modifier">
         <slot></slot>
     </div>
 </template>
 
 <script>
 export default{
+    props: ['modifier']
 }
 </script>
 
 <style scoped>
     div{
+        position: relative;
         background-color: rgb(0, 194, 0);
         padding: 20px;
         margin: 0 auto 20px auto;
         max-width: 800px;
         widows: 100%;
+        border: 1px solid black;
+    }
+    div .tabbed{
+        margin-top: 40px;
     }
 </style>
