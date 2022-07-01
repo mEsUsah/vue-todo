8b07998baee5051e3e983cfc1d9a3258a063cb94
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-07-02 00:26:22 +0200

    BaseCard: move margin to top

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -15,12 +15,12 @@ export default{
         position: relative;
         background-color: rgb(0, 194, 0);
         padding: 20px;
-        margin: 0 auto 20px auto;
+        margin: 20px auto 0 auto;
         max-width: 800px;
         widows: 100%;
         border: 1px solid black;
     }
     div .tabbed{
-        margin-top: 40px;
+        margin-top: 50px;
     }
 </style>
